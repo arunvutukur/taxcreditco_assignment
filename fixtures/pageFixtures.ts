ba16364b110c6df4c8cabd002e9c3f072a31cece
Arun <arunvaluemomentum@gmail.com>
2025-06-28 20:48:54 +0530

    Updated Page Fixtures

---
+++ fixtures/pageFixtures.ts
@@ -0,0 +1,26 @@
+
+import { test as base} from '@playwright/test'
+import { UserFormsPage } from '../page-objects/userFormsPage';
+import { TaxCreditFormPage } from '../page-objects/taxCreditFormPage';
+import { TaxAdditionalInformationPage } from '../page-objects/taxAdditionalInformationpage';
+export { expect } from "@playwright/test";
+ 
+export type TestOptions = {    
+    userFormsPage:UserFormsPage;  
+    taxCreditFormPage:TaxCreditFormPage;
+    taxAdditionalInformationPage:TaxAdditionalInformationPage;    
+}
+ 
+export const test = base.extend<TestOptions>({  
+ 
+    /** Page Implementation */
+    userFormsPage:async({page}, use) => {                
+        await use(new UserFormsPage(page));  
+    },
+    taxCreditFormPage:async({page}, use) => {                
+        await use(new TaxCreditFormPage(page));  
+    },
+    taxAdditionalInformationPage:async({page}, use) => {                
+        await use(new TaxAdditionalInformationPage(page));  
+    }
+});
